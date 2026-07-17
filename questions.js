851bff6622f1192a2298764c761a3647fb19e60b
growy525-dotcom <growy525@gmail.com>
2026-07-17 07:04:50 +0530

    Add questions array with sample questions

---
+++ questions.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    subject: "Odia",
+    question: "ଏଠାରେ ପ୍ରଥମ ପ୍ରଶ୍ନ ଆସିବ",
+    options: [
+      "Option A",
+      "Option B",
+      "Option C",
+      "Option D"
+    ],
+    answer: 0
+  },
+
+  {
+    id: 2,
+    subject: "English",
+    question: "Second Question",
+    options: [
+      "Option A",
+      "Option B",
+      "Option C",
+      "Option D"
+    ],
+    answer: 1
+  }
+
+];
